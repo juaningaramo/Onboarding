6640d7cdde13be0c5c895ad823621c21f391b537
Juan Ignacio Ingaramo <juani.river31@gmail.com>
2024-01-07 22:51:51 +0100

    Initial commit

---
+++ app/src/main/java/es/travelworld/onboarding/MainActivity.kt
@@ -0,0 +1,11 @@
+package es.travelworld.onboarding
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.xml_secundario)
+    }
+}
