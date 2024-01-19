103945f08e68a63e796b8c62eedd35279edd9ea6
Juan Ignacio Ingaramo <juani.river31@gmail.com>
2024-01-19 13:19:11 +0100

    uploaded proyect

--- app/src/main/java/es/travelworld/onboarding/MainActivity.kt
+++ app/src/main/java/es/travelworld/onboarding/MainActivity.kt
@@ -6,6 +6,8 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.xml_secundario)
+        setContentView(R.layout.xml_principal)
+
+
     }
 }
